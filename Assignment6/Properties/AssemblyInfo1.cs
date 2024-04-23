da6599a306f61868b08d675cf4ef036a5fdc6612
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-23 18:06:12 +0200

    Added AssemblyInfo and About form

---
+++ Assignment6/Properties/AssemblyInfo1.cs
@@ -0,0 +1,34 @@
+/// <summary>
+/// Filename: AssemblyInfo.cs
+/// Created on: 2024-04-23 00:00:00
+/// Author: Samuel Jeffman
+/// </summary>
+/// 
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("3619c796-4a78-4364-9b5b-c76b6cfa7ced")]
+
+[assembly: AssemblyTitle("Re-Minder")]
+[assembly: AssemblyDescription("Application to keep track of tasks you need to do. Created and maintained by Samuel Jeffman.")]
+[assembly: AssemblyCompany("-")]
+[assembly: AssemblyProduct("Re-Minder 2024")]
+[assembly: AssemblyCopyright("No Rights Reserved, Copyright © 2024")]
+[assembly: AssemblyTrademark("My trademark is No trademark")]
+[assembly: AssemblyVersion(version: "1.0.1.*")]
+[assembly: AssemblyFileVersion(version: "1.0.1")]
